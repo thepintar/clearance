e1525fdb88f3de526968f85335f5197e93c4b2f8
Michael Pintar <thepintar@gmail.com>
2016-03-28 22:51:31 -0500

    add clearance stuff

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+# config.action_mailer.raise_delivery_errors = true
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.gmail.com',
+  :domain         => 'mail.google.com',
+  :port           => 587,
+  :user_name      => 'foo@gmail.com',
+  :password       => '******',
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
